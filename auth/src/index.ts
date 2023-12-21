f0d67961a95cb987ba2770ca1cc615a797d6464d
Muhammad Jasim <jasimawan78@gmail.com>
2023-12-21 16:29:22 +0300

    ticketing service first commit

---
+++ auth/src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import { json } from "body-parser";
+import {
+  currentUserRouter,
+  signinRouter,
+  signoutRouter,
+  signupRouter,
+} from "./routes";
+
+const app = express();
+app.use(json());
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
