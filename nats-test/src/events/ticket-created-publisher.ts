7aa6cb339b2974e87fa99c15d9ebfa12f188ec75
Muhammad Jasim <jasimawan78@gmail.com>
2024-01-09 12:18:49 +0300

    nats-test removed

--- nats-test/src/events/ticket-created-publisher.ts
+++
@@ -1,7 +0,0 @@
-import { Publisher } from "./base-publisher";
-import { TicketCreatedEvent } from "./TicketCreatedEvent";
-import { Subjects } from "./subjects";
-
-export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
-  readonly subject = Subjects.TicketCreated;
-}
